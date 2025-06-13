d156b083aafcf475c081bec0d02fb04bfa866c9b
Ga Courel <gabrielgcourel@gmail.com>
2025-06-13 20:52:49 -0300

    caixinhaPronta

--- pomo.js
+++ pomo.js
@@ -1,13 +1,13 @@
-let segundos = 1500, tempo = 1500, counter = 0;
+let segundos = 1500, tempo = 1500, counterp = 0, counterc = 0, xfoco = 0;
 let shortRest = Math.floor(tempo / 5), longRest = Math.floor(tempo * (3 / 5));
 let tempoFoco = false, cronometro = false;
 let timer;
 
-function segundosParaTempo ()
+function segundosParaTempo (param)
 {
     const zeroEsquerda = (n) => Math.floor(n).toString().padStart(2, '0');
-    const min = zeroEsquerda (segundos / 60); 
-    const s = zeroEsquerda (segundos % 60);
+    const min = zeroEsquerda (param / 60); 
+    const s = zeroEsquerda (param % 60);
     return  `${min}:${s}`;
 }
 
@@ -16,7 +16,7 @@ function atualizarTempo() {
     const [minutos, segundosInput] = tempoInput.split(":").map(num => parseInt(num));
     
     // Verificar se a entrada tem formato válido de tempo MM:SS
-    if (!isNaN(minutos) && !isNaN(segundosInput) && minutos >= 0 && segundosInput >= 0 && segundosInput < 60) {
+    if (!isNaN(minutos) && !isNaN(segundosInput) && minutos >= 0 && segundosInput >= 0 && segundosInput < 60 && minutos <= 60) {
         segundos = minutos * 60 + segundosInput;
         tempo = segundos;
         shortRest = Math.floor(tempo / 5);
@@ -24,13 +24,19 @@ function atualizarTempo() {
         atualizarDisplay();
     } else {
         // Caso o formato seja inválido, mantemos o tempo original
-        alert("Formato inválido. Use MM:SS.");
+        alert("Formato inválido (use MM:SS). Tempo não pode ser maior que 1 hora");
     }
 }
 
 function atualizarDisplay() // Atualiza o conteúdo do elemento HTML 
 {
-    document.querySelector("#segundos").textContent = segundosParaTempo ();
+    document.querySelector("#segundos").textContent = segundosParaTempo (segundos);
+}
+
+function atualizarFoco ()
+{
+    const horario = segundosParaTempo (xfoco);
+    document.querySelector("#xtempo").textContent = `Você manteve o foco por ${horario}`;
 }
 
 function passaTempo ()
@@ -39,19 +45,29 @@ function passaTempo ()
     {
         segundos--;
         atualizarDisplay ();
+        xfoco++;
+        atualizarFoco ();
 
         if (segundos == 0)
         {
             tempoFoco = false;
-            counter++;
+            counterp++;
+            if (counterp == 1)
+            {
+                document.querySelector("#xpomodoro").textContent = `Você fez ${counterp} pomodoro`;
+            }
+            else
+            {
+                document.querySelector("#xpomodoro").textContent = `Você fez ${counterp} pomodoros`;
+            }
             //som de alarme
 
-            if (counter % 5 != 0)
+            if (counterp % 4 != 0)
             {
                 segundos = shortRest;
                 atualizarDisplay ();    
             }
-            else if (counter % 5 == 0)
+            else if (counterp % 4 == 0)
             {
                 segundos = longRest;
                 atualizarDisplay ();
@@ -70,6 +86,19 @@ function passaTempo ()
             //som de alarme
             segundos = tempo;
             atualizarDisplay ();
+
+            if (counterp % 4 == 0)
+            {
+                counterc++;
+                if (counterc == 1)
+                {
+                document.querySelector("#xciclos").textContent = `Você concluiu ${counterc} ciclo`;
+                }
+                else
+                {
+                document.querySelector("#xciclos").textContent = `Você concluiu ${counterc} ciclos`;
+                }
+            }
         }
         alterarModo ();
     }
@@ -137,28 +166,38 @@ function alterarModo ()
 {
     const body = document.body;
     const pomodoro = document.querySelector('.pomodoro');
+    const caixinha = document.querySelector("#caixinha");
     const botaop = document.querySelector("#botaoplay");
     const botaodc = document.querySelector("#botao1");
     const botaof = document.querySelector("#botao2");
     const botaodl = document.querySelector("#botao3");
+    const uai = document.querySelector("#instructions");
 
     if (tempoFoco)
     {
         body.classList.add('backgroundFoco');
-        pomodoro.classList.add('pomodoroColor');
-        botaop.classList.add('buttonColor', 'playFoco');
-        botaodc.classList.add('buttonColor');
-        botaof.classList.add('buttonColor');
-        botaodl.classList.add('buttonColor');
+        botaop.classList.add('playFoco');
+        pomodoro.style.backgroundColor = 'var(--roxo-escurinho)';
+        caixinha.style.backgroundColor = "var(--roxo-clarinho)";
+        botaop.style.backgroundColor = 'var(--roxo-escurinho)';
+        botaodc.style.backgroundColor = 'var(--roxo-escurinho)';
+        botaof.style.backgroundColor = 'var(--roxo-escurinho)';
+        botaodl.style.backgroundColor = 'var(--roxo-escurinho)';
+        uai.style.backgroundColor = "var(--roxo-clarinho)";
+        document.querySelector("#xmodo").textContent = `Modo de foco!`;
     }
     else
     {
         body.classList.remove('backgroundFoco');
-        pomodoro.classList.remove('pomodoroColor');
-        botaop.classList.remove('buttonColor', 'playFoco');
-        botaodc.classList.remove('buttonColor');
-        botaof.classList.remove('buttonColor');
-        botaodl.classList.remove('buttonColor');
+        botaop.classList.remove('playFoco');
+        pomodoro.style.backgroundColor = 'var(--azul-escurinho)';
+        caixinha.style.backgroundColor = "var(--azul-clarinho)";
+        botaop.style.backgroundColor = 'var(--azul-escurinho)';
+        botaodc.style.backgroundColor = 'var(--azul-escurinho)';
+        botaof.style.backgroundColor = 'var(--azul-escurinho)';
+        botaodl.style.backgroundColor = 'var(--azul-escurinho)';
+        uai.style.backgroundColor = "var(--azul-clarinho)";
+        document.querySelector("#xmodo").textContent = `Modo de descanso!`;
     }
 }
 /* alteração do CSS modo de foco */
